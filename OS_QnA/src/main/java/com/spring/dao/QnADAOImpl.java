34dc948fdbeff4049dd22ba110281e65d42143e8
sesamemanju36@gmail.com <sesamemanju36@gmail.com>
2024-04-09 11:25:30 +0900

    mapper,dao,context,sqlconfig 생성..!

--- OS_QnA/src/main/java/com/spring/dao/QnADAOImpl.java
+++ OS_QnA/src/main/java/com/spring/dao/QnADAOImpl.java
@@ -3,6 +3,7 @@ package com.spring.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 
 import com.spring.command.PageMaker;
@@ -14,49 +15,50 @@ public class QnADAOImpl implements QnADAO{
 	public void setSqlSession(SqlSession session) {
 		this.session = session;
 	}
-	
 	@Override
 	public List<QnAVO> selectSearchQnaList(PageMaker pageMaker) throws SQLException {
-		return session.selectList("QnA-Mapper.selectSearchQnaList",pageMaker);
+		int offset = pageMaker.getStartRow();
+		int limit = pageMaker.getPerPageNum();
+		
+		RowBounds rows = new RowBounds(offset, limit);
+		return session.selectList("QnA-Mapper.selectSearchQnaList", pageMaker, rows);
 	}
-	
-	@Override
-	public void insertQnA(QnAVO qna) throws SQLException {
-		session.insert("QnA-Mapper.insertQnA",qna);	
-	}
-
-	@Override
-	public void updateQnA(QnAVO qna) throws SQLException {
-		session.update("QnA-Mapper.updateQnA",qna);		
-	}
-
-	@Override
-	public void increaseViewCnt(QnAVO qna) throws SQLException {
-		//이 이거 맞나요
-	}
-
 	@Override
 	public int selectSearchQnAListCount(PageMaker pageMaker) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return session.selectOne("QnA-Mapper.selectSearchQnAListCount", pageMaker);
 	}
-
 	@Override
 	public QnAVO selectQnAByQnAId(int qnaid) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteQnA(int qnaid) throws SQLException {
-		// TODO Auto-generated method stub
 		
+		return session.selectOne("QnA-Mapper.selectQnAByQnAId", qnaid);
 	}
-
 	@Override
 	public int selectQnASeqNext() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return session.selectOne("QnA-Mapper.selectQnASeqNext");
 	}
+	@Override
+	public void insertQnA(QnAVO qna) throws SQLException {
+		session.insert("QnA-Mapper.insertQnA",qna);
+		
+	}
+	@Override
+	public void updateQnA(QnAVO qna) throws SQLException {
+		session.update("QnA-Mapper.updateQnA",qna);
+		
+	}
+	@Override
+	public void increaseViewCnt(int qnaid) throws SQLException {
+		session.update("QnA-Mapper.increaseViewCnt",qnaid);
+		
+	}
+	@Override
+	public void deleteQnA(int qnaid) throws SQLException {
+		session.delete("QnA-Mapper.deleteQnA",qnaid);
+		
+	}
+	
+
 
 }
