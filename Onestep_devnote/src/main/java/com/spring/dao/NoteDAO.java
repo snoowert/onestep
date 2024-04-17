d099f3c35abbd4762772c4004d907e9c39cca383
nkhy01@gmail.com <nkhy01@gmail.com>
2024-04-17 17:50:38 +0900

    test

--- Onestep_devnote/src/main/java/com/spring/dao/NoteDAO.java
+++ Onestep_devnote/src/main/java/com/spring/dao/NoteDAO.java
@@ -8,13 +8,13 @@ import com.spring.dto.NoteVO;
 public interface NoteDAO {
 	
 	
-	 public List<NoteVO> selectNoteList() throws SQLException;
-	 public NoteVO selectNoteByNoteId(int noteId) throws SQLException;
-	 public void insertNote(NoteVO note) throws SQLException;
-	 public void updateNote(NoteVO note) throws SQLException;
-	 public void deleteNote(int noteId) throws SQLException;
+	 List<NoteVO> selectNoteList() throws SQLException;
+	 NoteVO selectNoteByDnId(int noteId) throws SQLException;
+	 void insertNote(NoteVO note) throws SQLException;
+	 void updateNote(NoteVO note) throws SQLException;
+	 void deleteNote(int noteId) throws SQLException;
 	 
-	 public void increaseNoteViewPoint(int noteId) throws SQLException;
+	 void increaseNoteViewPoint(int noteId) throws SQLException;
 	 
 	 
 	 
